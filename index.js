33689928ccbc4447eef839b52f1367e276d078c4
Bomsiwakorn <bomsiwakorns@gmail.com>
2021-03-01 15:59:33 +0700

    Create TodoList:

---
+++ index.js
@@ -0,0 +1,18 @@
+function addTodo() {
+    let input = document.getElementById('inputTodo').value
+    let inputText = document.createTextNode(input)
+    if (input === '') {
+        alert('Invalid')
+    } else {
+        let list = document.createElement("li")
+        list.appendChild(inputText)
+        document.getElementById("todo").appendChild(list)
+        document.getElementById('inputTodo').value = ''
+    }
+}
+
+function clearTodo() {
+    let allTodo = document.getElementById('todo')
+    allTodo.innerHTML = ''
+
+}
