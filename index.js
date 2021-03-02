fe65943165f0ebd8403446734c15baa982102208
Bomsiwakorn <bomsiwakorns@gmail.com>
2021-03-02 15:12:21 +0700

    Delete TodoFunction

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-let addTodo = () => {
-    let input = document.getElementById('inputTodo').value
-    let inputText = document.createTextNode(input)
-    if (input === '') {
-        alert('Invalid')
-    } else {
-        let list = document.createElement("li")
-        list.appendChild(inputText)
-        document.getElementById("todo").appendChild(list)
-        document.getElementById('inputTodo').value = ''
-    }
-}
+
 
